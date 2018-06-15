1b903cc480365e233796b007bf1df0202043a43d
HABIMANA Jean de Dieu <32220586+Philos250@users.noreply.github.com>
2018-06-15 06:53:32 -0700

    Add files via upload

---
+++ ckeditor/config.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+config.filebrowserBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=files';
+   config.filebrowserImageBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=images';
+   config.filebrowserFlashBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=flash';
+   config.filebrowserUploadUrl = '../kcfinder/browse.php?opener=ckeditor&type=files';
+   config.filebrowserImageUploadUrl = '../kcfinder/browse.php?opener=ckeditor&type=images';
+   config.filebrowserFlashUploadUrl = '../kcfinder/browse.php?opener=ckeditor&type=flash';
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
